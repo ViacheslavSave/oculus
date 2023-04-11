45e7388b638b13f853129c509ba046ec92c60b19
ViacheslavSave <nikolya-ivanov-9191@mail.ru>
2023-04-11 18:36:08 +0300

    секция features готова

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-$(function(){
-
+const tabs = document.querySelectorAll(".tabs__item");
+const contentTabs = document.querySelectorAll(".content-tabs__item");
+tabs.forEach((element) => {
+	element.addEventListener("click", change);
 });
+function change(e) {
+	console.log(e.target.dataset.button);
+	tabs.forEach((element) => {
+		element.classList.remove("tabs__item--active");
+		e.target.classList.add("tabs__item--active");
+	});
+	contentTabs.forEach((element) => {
+		element.classList.remove("content-tabs__item--active");
+		document.querySelector(`#${e.target.dataset.button}`).classList.add("content-tabs__item--active");
+	});
+}
